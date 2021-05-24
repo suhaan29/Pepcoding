8ec8f9cb799a88f96a2932598353970c3cd88343
suhaan <suhaan2909@gmail.com>
2021-05-24 20:20:25 +0530

    saddle Price

---
+++ SaddlePrice.java
@@ -0,0 +1,41 @@
+package pepcoding.arrayQ;
+
+public class SaddlePrice {
+	
+	public static void main(String[] args)
+	{
+		int[][] arr = 
+				
+			  { { 11, 12, 13 , 14, 15 , 16 },
+				{ 21, 22, 23 , 24, 25 , 26 },
+				{ 31, 32, 33 , 34, 35 , 36 },
+				{ 41, 42, 43 , 44, 45 , 46 },
+				{ 51, 52, 53 , 54, 55 , 56 },
+				{ 61, 62, 63 , 64, 65 , 66 },
+				
+		};
+		
+		int max = 0;
+		
+		for(int i = 0; i < arr.length; i++)
+		{ 
+			int min = arr[i][0];
+			for(int j = 0; j < arr[0].length; j++)
+			{
+				if(arr[i][j] < min)
+				{
+					min = arr[i][j];
+				}
+			
+			}
+			
+			if(min > max)
+			{
+				max = min;
+			}
+		}
+		
+		System.out.println(max);
+	}
+
+}
