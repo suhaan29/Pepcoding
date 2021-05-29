70fb653f8fab9740ab25d72e504848b45e350fd8
suhaan <suhaan2909@gmail.com>
2021-05-29 08:18:22 +0530

    this is the one

---
+++ StringWithADifference.java
@@ -0,0 +1,28 @@
+package pepcoding.arrayQ;
+
+import java.util.Scanner;
+
+public class StringWithADifference {
+	
+	
+	public static void main(String[] args)
+	{
+		Scanner scn = new Scanner(System.in);
+		
+		StringBuilder s = new StringBuilder(scn.next());
+		StringBuilder d = new StringBuilder();
+		
+		int n = s.length();
+		
+		for(int i = 0; i < n-1; i++)
+		{
+			int a = (int)s.charAt(i) - (int)s.charAt(i+1);
+			d.append(s.charAt(i));
+			d.append(a);
+			
+		}
+		d.append(s.charAt(n-1));
+		System.out.println(d);
+		
+	}
+}
