a8ac646c9a9aca328e7fdd1b2839122909894a9d
suhaan <suhaan2909@gmail.com>
2021-05-30 09:11:21 +0530

    anotehr one

---
+++ RecursionPrintIncreasing.java
@@ -0,0 +1,19 @@
+package pepcoding.arrayQ;
+
+public class RecursionPrintIncreasing {
+	
+	static void printIncreasing(int n) {
+		
+		if(n == 0) {
+			return;
+		}
+		printIncreasing(n-1);
+		System.out.println(n);
+	}
+
+	public static void main(String[] args) {
+		int n = 5;
+		printIncreasing(5);
+	}
+	
+}
